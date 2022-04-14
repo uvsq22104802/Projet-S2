1adf2b8feda7d3b98f2fcabd98816edf27148acd
uvsq22104802 <pierre.striebig@ens.uvsq.fr>
2022-04-14 21:37:49 +0100

    popopo

--- Projetcode.py
+++ Projetcode.py
@@ -1,22 +1,28 @@
-from time import sleep
+from ast import Delete
 import tkinter as tk 
 import random as rd
-import asyncio 
 
 COTE = 300
 
-n = 10
+n = 100
 
 
 
 def crée_proie() :
+    global randomx, randomy
     for i in range (n) :
        randomx = rd.randint(0,290)
        randomy = rd.randint(0,290)
        proie = canvas.create_rectangle(randomx,randomy,randomx+10,randomy+10,fill="blue")
-        
-  
+
+       coord = canvas.coords(proie)
+
+       choc = canvas.find_overlapping(coord)
+       for obj in choc : 
+        canvas.itemconfigure(obj, fill = "red")
+
     
+
 
     return[proie]
 
@@ -32,5 +38,6 @@ canvas = tk.Canvas(racine, bg="black", width=COTE, height=COTE)
 canvas.grid()
 
 boloss = crée_proie()
+
 
 racine.mainloop()
