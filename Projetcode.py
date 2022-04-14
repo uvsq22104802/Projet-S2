f4b3fbb29d1b8a427a77cab69575446ecb7d0dfb
uvsq22104802 <pierre.striebig@ens.uvsq.fr>
2022-04-14 13:25:08 +0100

    projet de malade

--- Projetcode.py
+++ Projetcode.py
@@ -1,7 +1,32 @@
-for i in range (3):
- notes = int(input( "entrez note"))
- list.append(notes)
- 
+import tkinter as tk 
+import random as rd
 
-moyenne = sum(list)/len(list)
-print(list , moyenne)
+COTE = 300 
+
+randomx = rd.randint(0,290)
+randomy = rd.randint(0,290)
+
+
+def crée_proie() :
+    proie = canvas.create_oval(randomx,randomy,randomx+10,randomy+10,fill="blue")
+
+    return[proie]
+    
+
+
+
+
+
+
+#Creation du canvas#
+
+racine = tk.Tk()
+canvas = tk.Canvas(racine, bg="black", width=COTE, height=COTE)
+canvas.grid()
+
+boloss = crée_proie()
+
+racine.mainloop()
+
+
+
