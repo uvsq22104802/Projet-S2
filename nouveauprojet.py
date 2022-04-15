30e0ac8a27daacc1bb48f97af0b6c7903d4a77dd
uvsq22104802 <pierre.striebig@ens.uvsq.fr>
2022-04-15 14:15:58 +0100

    test

--- nouveauprojet.py
+++ nouveauprojet.py
@@ -7,6 +7,9 @@ GRILLE = 30
 n = 10
 CARRE = COTE/GRILLE
 
+def tour(event):
+    print("test")
+    
 
 def crée_proie() :
     global proies, randomx, randomy
@@ -14,7 +17,9 @@ def crée_proie() :
     for i in range (0,n) :
        randomx = rd.randint(0,GRILLE-1)
        randomy = rd.randint(0,GRILLE-1)
+       vie = 5
        canvas.create_rectangle(randomx*CARRE,randomy*CARRE,randomx*CARRE+CARRE,randomy*CARRE+CARRE,fill="blue")
+       tpl = 
 
     
 
@@ -28,18 +33,18 @@ def quadrillage() :
      canvas.create_line(i,COTE,i,0, fill= "white")
 
 #Creation du canvas#
-
 racine = tk.Tk()
 canvas = tk.Canvas(racine, bg="black", width=COTE, height=COTE)
 canvas.grid()
 
 #bouton
-
-
-
+bt = tk.Button(racine,text="nouveau tour")
+bt.grid()
+bt.bind('<Button-1>', tour)
 
 quadrillage()
 
 test = crée_proie()
+print(proies)
 
 racine.mainloop()
