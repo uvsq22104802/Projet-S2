f20eee75f0088278ce9c136290cca2b2160e8ccf
uvsq22104802 <pierre.striebig@ens.uvsq.fr>
2022-04-18 01:28:47 +0200

    whoau

--- test.py
+++ test.py
@@ -1,20 +1,82 @@
-import tkinter as tk
+import tkinter as tk 
+import random as rd
+import time
 
-i = 0
+#definition du canvas
+COTE = 600
+GRILLE = 30
+CARRE = COTE//GRILLE
 
-def init_value(evt):
-    global i
-    i = 0
-    
-def increase_value():
-    global i
-    print(i)
-    i += 1
-    
+#definition des regles de vie 
+life = 5
+n = 900
+reproduction = 3
 
-bt = tk.Button(text="press", command=increase_value, repeatdelay=20, repeatinterval=50)
-bt.pack()
 
-bt.bind('<ButtonRelease-1>', init_value)
+def tour(event):
+    global position,vie 
 
-tk.mainloop()
+    for i in range (reproduction) :
+       randomx = rd.randint(0,GRILLE-1)
+       randomy = rd.randint(0,GRILLE-1)
+
+       while [randomx, randomy] in position : 
+            randomx = rd.randint(0,GRILLE-1)
+            randomy = rd.randint(0,GRILLE-1)
+
+       canvas.create_rectangle(randomx*CARRE,randomy*CARRE,randomx*CARRE+CARRE,randomy*CARRE+CARRE,fill="blue")
+       position.append([randomx, randomy])
+       vie.append(life)
+       
+
+
+
+def crée_proie() :
+    global position, vie
+    position = []
+    vie = []
+
+    for i in range (0,n) :
+       randomx = rd.randint(0,GRILLE-1)
+       randomy = rd.randint(0,GRILLE-1)
+
+       while [randomx, randomy] in position : 
+            randomx = rd.randint(0,GRILLE-1)
+            randomy = rd.randint(0,GRILLE-1)
+
+       canvas.create_rectangle(randomx*CARRE,randomy*CARRE,randomx*CARRE+CARRE,randomy*CARRE+CARRE,fill="blue")
+       position.append([randomx, randomy])
+       vie.append(life)
+
+    return position
+
+
+
+
+
+
+#creation du quadrillage#
+
+def quadrillage() :
+    for i in range (0,COTE,CARRE) : 
+     canvas.create_line(COTE,i,0,i, fill="white")
+     canvas.create_line(i,COTE,i,0, fill= "white")
+
+
+
+#Creation du canvas#
+racine = tk.Tk()
+canvas = tk.Canvas(racine, bg="black", width=COTE, height=COTE)
+canvas.grid()
+
+#bouton
+bt = tk.Button(racine,text="nouveau tour")
+bt.grid()
+bt.bind('<Button-1>', tour)
+
+quadrillage()
+
+test = crée_proie()
+print(position)
+
+racine.mainloop()
