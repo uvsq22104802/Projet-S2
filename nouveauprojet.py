c897b23d560a2fd0553d6a024f18a680f13fa067
uvsq22104802 <pierre.striebig@ens.uvsq.fr>
2022-04-19 14:12:48 +0100

    nouveau code

--- nouveauprojet.py
+++ nouveauprojet.py
@@ -1,67 +1,65 @@
+from calendar import c
 import tkinter as tk 
 import random as rd
+from random import randint, choice
 
 #definition du canvas
 COTE = 600
 GRILLE = 30
 CARRE = COTE//GRILLE
+grille  = {}
+proies =  {}
 
 #definition des regles de vie 
 life = 5
 n = 10
-reproduction = 330
+reproduction = 5
 
 
 def tour(event):
-    global position,vie 
-    mort_deplacement()
+    mort()
 
-    for i in range (reproduction) :
-       randomx = rd.randint(0,GRILLE-1)
-       randomy = rd.randint(0,GRILLE-1)
+    for p in range(reproduction):
+        coord = choice(list(grille))
+        grille.pop(coord)
+        canvas.create_rectangle(coord[0]*CARRE,coord[1]*CARRE,coord[0]*CARRE+CARRE,coord[1]*CARRE+ CARRE, fill = "blue")
+        proies[coord] = life
 
-       while [randomx, randomy] in position : 
-            randomx = rd.randint(0,GRILLE-1)
-            randomy = rd.randint(0,GRILLE-1)
 
-       canvas.create_rectangle(randomx*CARRE,randomy*CARRE,randomx*CARRE+CARRE,randomy*CARRE+CARRE,fill="blue")
-       position.append([randomx, randomy])
-       vie.append(life)
+#creer proies
+def creer_proies():
+    for p in range(n):
+        coord = choice(list(grille))
+        grille.pop(coord)
+        canvas.create_rectangle(coord[0]*CARRE,coord[1]*CARRE,coord[0]*CARRE+CARRE,coord[1]*CARRE+ CARRE, fill = "blue")
+        proies[coord] = 5
     
 
 
-def crée_proie() :
-    global position, vie
-    position = []
-    vie = []
-
-    for i in range (n) :
-       randomx = rd.randint(0,GRILLE-1)
-       randomy = rd.randint(0,GRILLE-1)
-
-       while [randomx, randomy] in position : 
-            randomx = rd.randint(0,GRILLE-1)
-            randomy = rd.randint(0,GRILLE-1)
-
-       canvas.create_rectangle(randomx*CARRE,randomy*CARRE,randomx*CARRE+CARRE,randomy*CARRE+CARRE,fill="blue")
-       position.append([randomx, randomy])
-       vie.append(life)
-
-    
-
-
-def mort_deplacement() :
-    for j in range (len(vie)) :
-        vie[j] -= 1
-        
-        if vie[j] == 0 :
-            mourir = canvas.find_overlapping(position[j][0]*CARRE,position[j][1]*CARRE,position[j][0]*CARRE+CARRE,position[j][1]*CARRE+CARRE)
-            for obj in mourir :
+def mort() :
+    print(proies)
+    a_delete = []
+    for p in proies:
+        proies[p] -= 1
+        if proies[p] == 0:
+            suppr = canvas.find_overlapping(p[0]*CARRE,p[1]*CARRE,p[0]*CARRE+CARRE,p[1]*CARRE+CARRE)
+            for obj in suppr : 
                 canvas.delete(obj)
+            a_delete.append(p)
+            grille[p] = 0
+            
+    for position in a_delete:
+        proies.pop(position)
+
+
 
 
         
-        
+#definition de toute les positions
+def creer_grille():
+    for i in range(30):
+        for j in range(30):
+            grille[(i,j)] = 0
 
 
 
@@ -75,10 +73,9 @@ bt = tk.Button(racine,text="nouveau tour")
 bt.grid()
 bt.bind('<Button-1>', tour)
 
+creer_grille()
 
-test = crée_proie()
+creer_proies()
 
-
-print(position)
 
 racine.mainloop()
